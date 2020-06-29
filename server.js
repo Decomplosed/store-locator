eefe781eff52f88e67ea57625b8225ba83841c4c
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-29 23:15:37 +0200

    run app on PORT

--- server.js
+++ server.js
@@ -9,3 +9,7 @@ dotenv.config({ path: './config/config.env' })
 const app = express()
 
 const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NOVE_ENV} mode on port ${PORT}`)
+})
