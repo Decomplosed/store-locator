4a296633beed797b9e855468d81a555ec84c07bd
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-29 23:17:01 +0200

    log the information about the fact that server started running

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NOVE_ENV} mode on port ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 })
