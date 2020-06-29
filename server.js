f420dd170c725294a14dcf20d27ab2d6943789fc
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-29 23:11:45 +0200

    laod env variables

--- server.js
+++ server.js
@@ -2,3 +2,6 @@ const path = reuire('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
+
+// Load env vars
+dotenv.config({ path: './config/config.env' })
