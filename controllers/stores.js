fe5b6a6255056833c10d9aabe7e28af2e5db6c88
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-30 08:09:45 +0200

    return 200 status and log the data added and success information

--- controllers/stores.js
+++ controllers/stores.js
@@ -24,6 +24,11 @@ exports.getStores = async (req, res, next) => {
 exports.addStore = async (req, res, next) => {
   try {
     const store = await Store.create(req.body)
+
+    return res.status(200).json({
+      success: true,
+      data: store,
+    })
   } catch (err) {
     console.error(err)
     res.status(500).json({ error: 'Server Error' })
