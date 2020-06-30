7e16132aafe3dd4c8b2e83b6b77a41ca13981580
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-30 07:48:30 +0200

    once connected log information about it

--- config/db.js
+++ config/db.js
@@ -8,5 +8,7 @@ const connectDB = async () => {
       useFindAndModify: false,
       useUnifiedTopology: true,
     })
+
+    console.log(`MongoDB connected: ${conn.connection.host}`)
   } catch (err) {}
 }
