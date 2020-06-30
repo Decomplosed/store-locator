abb8142ffbd6aa6ca39def79d043d236caf9415d
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-30 07:59:35 +0200

    turned getStores to be async

--- controllers/stores.js
+++ controllers/stores.js
@@ -3,6 +3,6 @@ const Store = require('../models/Store')
 // @desc  GET all stores
 // @route GET /api/v1/stores
 // @access Public
-exports.getStores = (req, res, next) => {
-  res.send('Hello')
+exports.getStores = async (req, res, next) => {
+  
 }
