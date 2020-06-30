2731a854907fdc61121aa65b23a66dafd3d0cd51
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-30 07:49:45 +0200

    imported connectDB

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
+const connectDB = require('./config/db')
 
 // Load env vars
 dotenv.config({ path: './config/config.env' })
