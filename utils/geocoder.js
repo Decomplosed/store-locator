ae9c1aba0761fc33827e4d2a22b6c2521c8be706
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-01 09:07:06 +0200

    setup options for geocoder

--- utils/geocoder.js
+++ utils/geocoder.js
@@ -1,1 +1,8 @@
 const NodeGeocoder = require('node-geocoder')
+
+const options = {
+  provider: pricess.env.GEOCODER_PROVIDER,
+  httpAdapted: 'https',
+  apiKey: process.env.GEOCODER_API_KEY,
+  formatter: null,
+}
