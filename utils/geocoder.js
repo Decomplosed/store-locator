14a2e477cb848893473159520eb54c098c18140b
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-01 09:10:01 +0200

    fixed typo

--- utils/geocoder.js
+++ utils/geocoder.js
@@ -1,7 +1,7 @@
 const NodeGeocoder = require('node-geocoder')
 
 const options = {
-  provider: pricess.env.GEOCODER_PROVIDER,
+  provider: process.env.GEOCODER_PROVIDER,
   httpAdapted: 'https',
   apiKey: process.env.GEOCODER_API_KEY,
   formatter: null,
