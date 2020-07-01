83975c8f2e25593b12a1e432a5323760ed1f5d78
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-01 11:08:07 +0200

    started configuring map

--- public/js/map.js
+++ public/js/map.js
@@ -6,3 +6,32 @@ const map = new mapboxgl.Map({
   zoom: 9,
   center: [-71.157895, 42.707741],
 })
+
+function loadMap() {
+  map.on('load', function () {
+    map.addSource('point', {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: [
+          {
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [-71.157895, 42.707741],
+            },
+          },
+        ],
+      },
+    })
+    map.addLayer({
+      id: 'points',
+      type: 'symbol',
+      source: 'point',
+      layout: {
+        'icon-image': 'cat',
+        'icon-size': 0.25,
+      },
+    })
+  })
+}
