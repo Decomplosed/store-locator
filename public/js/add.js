c840a0b8fd8ae4544ce5831dc8b871034e92f712
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-02 10:18:18 +0200

    prevent default behaviour

--- public/js/add.js
+++ public/js/add.js
@@ -2,6 +2,9 @@ const storeForm = document.getElementById('store-form')
 const storeId = document.getElementById('store-id')
 const storeAddress = document.getElementById('store-address')
 
-function addStore() {}
+// Send POST to API to add store
+function addStore(e) {
+  e.preventDefault()
+}
 
 storeForm.addEventListener('submit', addStore)
