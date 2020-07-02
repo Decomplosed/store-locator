ade0e32877737ef28d47c6e07e04b8f33669fe2e
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-02 10:17:21 +0200

    created event listener for form - submit

--- public/js/add.js
+++ public/js/add.js
@@ -1,3 +1,5 @@
 const storeForm = document.getElementById('store-form')
 const storeId = document.getElementById('store-id')
 const storeAddress = document.getElementById('store-address')
+
+storeForm.addEventListener('submit', addStore)
