6eea01b31b276d81cd6b050bc351ce718a545f82
Bart Frackiewicz <decomplosed@gmail.com>
2020-07-02 10:17:41 +0200

    defined addStore function

--- public/js/add.js
+++ public/js/add.js
@@ -2,4 +2,6 @@ const storeForm = document.getElementById('store-form')
 const storeId = document.getElementById('store-id')
 const storeAddress = document.getElementById('store-address')
 
+function addStore() {}
+
 storeForm.addEventListener('submit', addStore)
